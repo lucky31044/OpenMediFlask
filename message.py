4518823562c8c3aa1b42a3ec7fc8dd4a333d5c84
ABHINAV JAIN <jain.abhinav14@gmail.com>
2022-10-07 18:41:28 +0530

    message

--- message.py
+++ message.py
@@ -7,31 +7,46 @@ os.environ['TWILIO_ACCOUNT_SID'] = ""
 os.environ['TWILIO_AUTH_TOKEN'] = ""
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
+import requests
+
+def send_msg(st,mobno):
+    
+    url = "https://api.twilio.com/2010-04-01/Accounts/AC691257116d7e79d5ef694cbb3771f512/Messages.json"
+
+    payload='From=whatsapp%3A%2B14155238886&Body='+st+'&To=whatsapp%3A%2B91'+mobno
+    headers = {
+    'Authorization': 'Basic QUM2OTEyNTcxMTZkN2U3OWQ1ZWY2OTRjYmIzNzcxZjUxMjplYTYwOGZjNzA0M2JkNjVjZWViMGVkNTUyMDkzZTcyMA==',
+    'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
 
 
 client = Client(account_sid, auth_token)
 
-def send_msg(st):
-    resp = MessagingResponse()
-    msg = resp.message()
-    msg.body(st)
+# def send_msg(st):
+#     resp = MessagingResponse()
+#     msg = resp.message()
+#     msg.body(st)
 
-    print("####### In Msg ########")
+#     print("####### In Msg ########")
     
-    responded = True
-    return resp
-    
-def send_template_msg(phone_id,st):
-    client = rest.Client(account_sid, auth_token)
-    message = client.messages.create(
-                                from_='whatsapp:+14155238886',
-                                body=st,
-                                to='whatsapp:+91'+phone_id
-                            )
+#     responded = True
+#     return resp
 
-    print("####### In template Msg ########")
-    print(st)
-    return Response("ok",status=200)
+# def send_template_msg(phone_id,st):
+#     client = rest.Client(account_sid, auth_token)
+#     message = client.messages.create(
+#                                 from_='whatsapp:+14155238886',
+#                                 body=st,
+#                                 to='whatsapp:+91'+phone_id
+#                             )
+
+#     print("####### In template Msg ########")
+#     print(st)
+#     return Response("ok",status=200)
 
 
 def send_file(phone_id,url,st='success'):
